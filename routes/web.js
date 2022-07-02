bb7b87c9d0766eb9eefee7bb4e814dc4d1ee9d5a
Shalini Swaraj <sanskarravi9408@gmail.com>
2022-07-02 23:53:53 +0530

    dynamic cart

---
+++ routes/web.js
@@ -0,0 +1,21 @@
+const homeController =require('../app/http/controllers/customer/homeController')
+const authController =require('../app/http/controllers/customer/authController')
+const cartController =require('../app/http/controllers/customer/cartController')
+
+ function initRoutes(app) {
+   
+    app.get('/', homeController().index)
+  
+
+    app.get('/login',authController().login)
+
+    app.get('/register', authController().register)
+   // (req, res) => {
+    //    res.render('auth/register')
+    //})
+
+    app.get('/cart',cartController().index)
+    app.post('/update-cart',cartController().update)
+}
+
+module.exports = initRoutes
--- server.js
+++ server.js
@@ -1,42 +1,53 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const ejs=require('ejs')
-const path=require('path')
-const  expressLayout=require('express-ejs-layouts')
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDBStore = require('connect-mongo')
 
-  /*  if(process.env.PORT){
-     PORT=process.env.PORT
-    }else{
-      PORT=3000
-    } 
- */
- 
-   app.use(express.static('public'))
+// Database connection
+mongoose.connect(process.env.MONGO_CONNECTION_URL , {useNewUrlParser:true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+});
 
-    // set Template engine
-    app.use(expressLayout)
-    app.set('views',path.join(__dirname,'/resources/views'))
-    app.set('view engine','ejs')
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  store: MongoDBStore.create({
+      mongoUrl: process.env.MONGO_CONNECTION_URL
+  }),
+  saveUninitialized: false,
+  cookie: { maxAge: 1000*60*60*24 } // 24 hours
 
-    
-    app.get('/',function(req,res){
-      res.render('home')
-    })
+}))
 
-    app.get('/cart',(req,res)=>{
-      res.render('customer/cart')
-    })
+app.use(flash())
 
-    app.get('/login',(req,res)=>{
-      res.render('auth/login')
-    })
+//Asserts
+app.use(express.static('public'))
 
-    app.get('/register',(req,res)=>{
-      res.render('auth/register')
-    })
-   
+app.use(express.json())
+
+//global middleware
+app.use((req,res,next)  =>{
+res.locals.session = req.session
+next()
+})
+
+// set Template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+require('./routes/web')(app)
 
 app.listen(PORT, () => {
-    console.log('Listening on port 3000')
+  console.log('Listening on port 3000')
 })
